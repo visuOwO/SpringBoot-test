3e836903f7f7b13378e0a6c7e33c4ccc3b159eb7
visuOwO <held18@outlook.com>
2022-06-27 18:10:19 +0800

    oauth2 test

--- src/main/java/com/example/hld/springbootdemo/security/AuthorizationServerConfig.java
+++ src/main/java/com/example/hld/springbootdemo/security/AuthorizationServerConfig.java
@@ -1,63 +1,50 @@
 package com.example.hld.springbootdemo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    AuthenticationManager authenticationManager;
 
-    // 该对象用来将令牌信息存储到内存中
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
     @Autowired(required = false)
-    TokenStore inMemoryTokenStore;
+    private UserService userService;
 
-    // 该对象将为刷新token提供支持
-    @Autowired
-    UserDetailsService userDetailsService;
-
-    // 指定密码的加密方式
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        // 使用BCrypt强哈希函数加密方案（密钥迭代次数默认为10）
-        return new BCryptPasswordEncoder();
+    /**
+     * 密码模式采用AuthorizationServerEndpointsConfigurer配置
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userService);
     }
 
-    // 配置 password 授权模式
+    /**
+     * @param clients
+     * @throws Exception
+     */
     @Override
-    public void configure(ClientDetailsServiceConfigurer clients)
-            throws Exception {
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("password")
-                .authorizedGrantTypes("password", "refresh_token") //授权模式为password和refresh_token两种
-                .accessTokenValiditySeconds(1800) // 配置access_token的过期时间
-                .resourceIds("rid") //配置资源id
+                .withClient("admin")
+                .secret(passwordEncoder.encode("112233"))
+                .accessTokenValiditySeconds(3600)
+                .redirectUris("https://www.baidu.com")
                 .scopes("all")
-                .secret("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq"); //123加密后的密码
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.tokenStore(inMemoryTokenStore) //配置令牌的存储（这里存放在内存中）
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService);
-    }
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) {
-        // 表示支持 client_id 和 client_secret 做登录认证
-        security.allowFormAuthenticationForClients();
+                //密码模式
+                .authorizedGrantTypes("password");
     }
 }
