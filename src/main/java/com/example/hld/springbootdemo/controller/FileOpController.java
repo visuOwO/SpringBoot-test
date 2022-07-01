38c28b07850835b448b501277751a2788c9d4002
visuOwO <held18@outlook.com>
2022-07-01 15:45:59 +0800

    remove db controller and add some other file api.

--- src/main/java/com/example/hld/springbootdemo/controller/FileOpController.java
+++ src/main/java/com/example/hld/springbootdemo/controller/FileOpController.java
@@ -1,87 +1,88 @@
 package com.example.hld.springbootdemo.controller;
 
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONArray;
-import com.alibaba.fastjson2.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static com.example.hld.springbootdemo.controller.OpenAS2CommandController.*;
 
 @Controller
 public class FileOpController {
-    public final static String DATA_PATH_PREFIX ="/opt/OpenAS2/test-data";
-    public final static String url = "https://localhost:8443/api/partnership/view/";
-
-    private String AS2_SERVER_API_URL = "https://localhost:8443/api";
+    public final static String OPENAS2_DATA_BASE_PATH="home/visu/OpenAS2-data";
+    public final static String DATA_PATH ="files";
+    public final static String CERT_DATA_PATH = "certs";
+    public final static String OPENAS2_SENDER = "senderIDs";
+    public final static String OPENAS2_RECEIVER = "receiverIDs";
 
     @RequestMapping("/file/upload")
-    public String uploadPage(HttpServletRequest request) {
+    public String uploadPage() {
         return "fileUploadPage";
     }
 
     @ResponseBody
     @RequestMapping(value="/file/receive", method= RequestMethod.POST)
-    public String receiveFile(@RequestParam("file")MultipartFile file, HttpServletRequest request) throws Exception {
+    public String receiveFile(@RequestParam("file")MultipartFile file) throws Exception {
         if (file.isEmpty()) {
             return "There's No File.";
         }
 
         //Use Directory Relative Path
-        String directRelativePath = "src/main/resources" + File.separator + DATA_PATH_PREFIX;
-        File directRelativeDir = new File(directRelativePath);
+        String filename = file.getOriginalFilename();
+        String absolutePath;
+        if (filename.endsWith(".p12") || filename.endsWith(".p7b") || filename.endsWith(".pfx") || filename.endsWith(".cer")) {
+            absolutePath = OPENAS2_DATA_BASE_PATH + File.separator + CERT_DATA_PATH;
+        }
+        else {
+            absolutePath = OPENAS2_DATA_BASE_PATH + File.separator + DATA_PATH;
+        }
+        File absoluteDir = new File(absolutePath);
 
-        if (!directRelativeDir.exists()) {
-            if (!directRelativeDir.mkdirs())
+        if (!absoluteDir.exists()) {
+            if (!absoluteDir.mkdirs())
                 throw new Exception("Unknown Exception when creating Directory.");
         }
-        String filename = file.getOriginalFilename();
-        File uploadFile = new File(directRelativeDir.getAbsolutePath() + File.separator + filename);
+        
+        File uploadFile = new File(absoluteDir.getAbsolutePath() + File.separator + filename);
         file.transferTo(uploadFile);
         return "File uploaded successfully!";
     }
 
-    @RequestMapping(value = "/file/download", method = RequestMethod.GET)
-    public void download(@RequestParam("filename") String filename, HttpServletResponse response) {
+    @ResponseBody
+    @RequestMapping(value = "/file/download/{resource}", method = RequestMethod.GET)
+    public String download(@PathVariable("resource") String resource,@RequestParam("filename") String filename, HttpServletResponse response) {
         try {
-            String directRelativePath = "src/main/resources" + File.separator + DATA_PATH_PREFIX + File.separator + filename;
-            File file = new File(directRelativePath);
-            String ext = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
-
+            String absolutePath;
+            if (Objects.equals(resource, DATA_PATH)) {
+                absolutePath = OPENAS2_DATA_BASE_PATH + File.separator + DATA_PATH + File.separator + filename;
+            } else if (Objects.equals(resource, CERT_DATA_PATH)) {
+                absolutePath = OPENAS2_DATA_BASE_PATH + File.separator + CERT_DATA_PATH + File.separator + filename;
+            } else {
+                return "Unknown resource";
+            }
+            File file = new File(absolutePath);
             FileInputStream fis = new FileInputStream(file);
-            InputStream is = new BufferedInputStream(fis);
-            byte[] buffer = new byte[is.available()];
-            is.read(buffer);
-            is.close();
-            response.reset();
-            response.setCharacterEncoding("UTF-8");
-            response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "utf-8"));
-            response.addHeader("Content-Length", "" + file.length());
-            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-            response.setContentType("application/octet-stream");
-            outputStream.write(buffer);
-            outputStream.flush();
+            downloadFile(filename, response, file, fis);
+            return "Success!";
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
 
     @ResponseBody
     @RequestMapping("/file/send/{connector}")
-    public String send(@PathVariable("connector") String connector, @RequestParam("filename") String filename, HttpServletRequest request) {
+    public String send(@PathVariable("connector") String connector, @RequestParam("filename") String filename) {
         //TODO move the file to the folder under the sender's AS2 ID
-        String sender = getAS2IDByConnectionName(connector,"senderIDs");
-        String receiver = getAS2IDByConnectionName(connector,"receiverIDs");
-        String baseUploadPath = "src/main/resources" + File.separator + DATA_PATH_PREFIX;
+        String sender = getAS2IDByConnectionName(connector,OPENAS2_SENDER);
+        String receiver = getAS2IDByConnectionName(connector,OPENAS2_RECEIVER);
+        String baseUploadPath = OPENAS2_DATA_BASE_PATH + File.separator + DATA_PATH;
         File f = new File(baseUploadPath + File.separator + filename);
         try (FileInputStream fis = new FileInputStream(f)) {
             InputStream is = new BufferedInputStream(fis);
@@ -107,167 +108,93 @@ public class FileOpController {
         }
     }
 
-    @RequestMapping("/file/download/{connector}")
-    public void download(@PathVariable("connector") String connector, HttpServletRequest response) {
-        //TODO download the file to the folder under the sender's AS2 ID
+    /**
+     * Download the file sent by a connection
+     * @param connection connection(partnership) name
+     * @param filename filename
+     */
+    @ResponseBody
+    @RequestMapping(value = "/file/downloadSentFile/{connection}", method = RequestMethod.GET)
+    public String downloadSentFile(@PathVariable("connection") String connection, @RequestParam("filename") String filename, HttpServletResponse response) {
+        String senderID = getAS2IDByConnectionName(connection, OPENAS2_SENDER);
+        String receiverID = getAS2IDByConnectionName(connection, OPENAS2_RECEIVER);
+        String absoluteSentPath = OPENAS2_DATA_BASE_PATH + File.separator + DATA_PATH + File.separator + senderID + "-" + receiverID + File.separator + "sent";
+        File f = new File(absoluteSentPath + File.separator + filename);
+        try (FileInputStream fis = new FileInputStream(f)) {
+            downloadFile(filename, response, f, fis);
+        } catch (IOException e) {
+            return "File not found" + filename;
+        }
+        return "Successfully download file: " + filename;
     }
 
-    private String getAS2IDByConnectionName(String partnership, String name) {
-        try {
-            URL requestURL = new URL(url+partnership);
-            HttpURLConnection conn = (HttpURLConnection) requestURL.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Authorization","Basic YWRtaW46MTIzNDU2");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
 
-            InputStream is = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String str = br.readLine();
-            if (str == null) return null;
-            is.close();
-            conn.disconnect();
-            JSONObject j = JSON.parseObject(str);
-            JSONArray results = j.getJSONArray("results");
-            JSONObject array = results.getJSONObject(0);
-            JSONObject recvID = array.getJSONObject(name);
-            return recvID.getString("as2_id");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+    @ResponseBody
+    @RequestMapping(value="/file/downloadReceivedFile/{connection}", method = RequestMethod.GET)
+    public String downloadReceivedFile(@PathVariable("connection") String connection, @RequestParam("filename") String filename, HttpServletResponse response) {
+        String senderID = getAS2IDByConnectionName(connection, OPENAS2_SENDER);
+        String receiverID = getAS2IDByConnectionName(connection, OPENAS2_RECEIVER);
+        String absoluteSentPath = OPENAS2_DATA_BASE_PATH + File.separator + DATA_PATH + File.separator + senderID + "-" + receiverID + File.separator + "received";
+        File f = new File(absoluteSentPath + File.separator + filename);
+        try (FileInputStream fis = new FileInputStream(f)) {
+            downloadFile(filename, response, f, fis);
+        } catch (IOException e) {
+            return "File not found" + filename;
         }
+        return "Successfully download file: " + filename;
+    }
+
+    private void downloadFile(@RequestParam("filename") String filename, HttpServletResponse response, File f, FileInputStream fis) throws IOException {
+        InputStream is = new BufferedInputStream(fis);
+        byte[] buffer = new byte[is.available()];
+        is.read(buffer);
+        is.close();
+        response.reset();
+        response.setCharacterEncoding("UTF-8");
+        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "utf-8"));
+        response.addHeader("Content-Length", "" + f.length());
+        OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+        response.setContentType("application/octet-stream");
+        outputStream.write(buffer);
+        outputStream.flush();
     }
 
     @ResponseBody
-    @RequestMapping(value = {"/api/{resource}/{action}/{id}","/api/{resource}/{action}"}, method = RequestMethod.POST)
-    public String postAs2Command(@PathVariable("resource") String resource, @PathVariable("action") String action, @PathVariable(value = "id",required = false) String itemId,
-                                @RequestParam Map<String, String> params) {
-        String urlName;
-        if (itemId == null) {
-            urlName = AS2_SERVER_API_URL + "/" + resource + "/" + action;
+    @RequestMapping(value = "/file/listSentFile/{connection}", method = RequestMethod.GET)
+    public List<String> listSentFile(@PathVariable("connection") String connection) {
+        String senderID = getAS2IDByConnectionName(connection, OPENAS2_SENDER);
+        String receiverID = getAS2IDByConnectionName(connection, OPENAS2_RECEIVER);
+        String absoluteSentPath = OPENAS2_DATA_BASE_PATH + File.separator + DATA_PATH + File.separator + senderID + "-" + receiverID + File.separator + "sent";
+        return readFiles(absoluteSentPath);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/file/listReceivedFile/{connection}", method = RequestMethod.GET)
+    public List<String> listReceivedFile(@PathVariable("connection") String connection) {
+        String senderID = getAS2IDByConnectionName(connection, OPENAS2_SENDER);
+        String receiverID = getAS2IDByConnectionName(connection, OPENAS2_RECEIVER);
+        String absoluteSentPath = OPENAS2_DATA_BASE_PATH + File.separator + DATA_PATH + File.separator + senderID + "-" + receiverID + File.separator + "received";
+        return readFiles(absoluteSentPath);
+    }
+
+    public List<String> readFiles(String path) {
+        List<String> fileList = new ArrayList<>();
+        File file = new File(path);
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File value : files) {
+                if (!value.isDirectory()) {
+                    String path1 = value.getPath();
+                    String filename = path1.substring(path1.lastIndexOf(File.separator) + 1);
+                    fileList.add(filename);
+                }
+            }
         }
         else {
-            urlName = AS2_SERVER_API_URL + "/" + resource + "/" + action + "/" + itemId;
+            String path1 = file.getPath();
+            String filename = path1.substring(path1.lastIndexOf(File.separator)+1);
+            fileList.add(filename);
         }
-        if (action.equalsIgnoreCase("importByFile")) {
-            return importByFileName(urlName,itemId,params);
-        }
-        try {
-            URL url = new URL(urlName);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Authorization","Basic dXNlcklEOnBXZA==");
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.connect();
-            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
-            Iterator<Map.Entry<String, String>> entries = params.entrySet().iterator();
-            while (entries.hasNext()) {
-                Map.Entry<String, String> entry = entries.next();
-                String s = entry.getKey() + "=" + entry.getValue();
-                dos.writeBytes(s);
-                if (entries.hasNext()) {
-                    dos.writeBytes("&");
-                }
-            }
-            dos.flush();
-            dos.close();
-
-            int resultCode = conn.getResponseCode();
-            if (HttpURLConnection.HTTP_OK == resultCode) {
-                InputStream is = conn.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                String str = br.readLine();
-                if (str == null) return null;
-                is.close();
-                conn.disconnect();
-                return str;
-            }
-            else {
-                return null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        return fileList;
     }
-
-
-    @ResponseBody
-    @RequestMapping(value = {"/api/{resource}/{action}/{id}","/api/{resource}/{action}"}, method = RequestMethod.GET)
-    public String getAs2Command(@PathVariable("resource") String resource, @PathVariable("action") String action, @PathVariable(value = "id",required = false) String itemId, HttpServletRequest request){
-        try {
-            String urlName;
-            if (itemId == null) {
-                urlName = AS2_SERVER_API_URL + "/" + resource + "/" + action;
-            }
-            else {
-                urlName = AS2_SERVER_API_URL + "/" + resource + "/" + action + "/" + itemId;
-            }
-            URL url = new URL(urlName);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestProperty("Authorization","Basic dXNlcklEOnBXZA==");
-            conn.setRequestMethod("GET");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.connect();
-            InputStream is = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String str = br.readLine();
-            if (str == null) return null;
-            is.close();
-            conn.disconnect();
-
-            return str;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private String importByFileName(String urlName, String alias, Map<String, String> params) {
-        try {
-            URL url = new URL(urlName);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Authorization", "Basic dXNlcklEOnBXZA==");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
-            Iterator<Map.Entry<String, String>> entries = params.entrySet().iterator();
-            while (entries.hasNext()) {
-                Map.Entry<String, String> entry = entries.next();
-                System.out.println(entry.getKey());
-                if (entry.getKey().equalsIgnoreCase("fileName")) {
-                    String fileName = DATA_PATH_PREFIX + "/" + entry.getValue();
-                    String s = entry.getKey() + "=" + fileName;
-                    dos.writeBytes(s);
-                }
-                else {
-                    String s = entry.getKey() + "=" + entry.getValue();
-                    dos.writeBytes(s);
-                }
-                if (entries.hasNext()) {
-                    dos.writeBytes("&");
-                }
-            }
-            InputStream is = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String str = br.readLine();
-            if (str == null) return null;
-            is.close();
-            conn.disconnect();
-            return str;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
 }
